17da5393cb1e9ad5a778219b3fa8b02513c149e6
Ali <alii.torabi@gmail.com>
2018-10-08 23:53:33 +0000

    initial commit

---
+++ first_ros_WS/devel/setup.bash
@@ -0,0 +1,1 @@
+/home/ali/ROS/first_ros_WS/devel/.private/catkin_tools_prebuild/setup.bash
---
+++ first_ros_WS/devel/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
@@ -0,0 +1,1 @@
+/home/ali/ROS/first_ros_WS/devel/.private/catkin_tools_prebuild/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
