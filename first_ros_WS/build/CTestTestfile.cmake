39d0616d2a506cbaa387fa99b8aa7a275a141fc5
Ali <alii.torabi@gmail.com>
2018-10-08 19:33:41 -0600

    initial commit

---
+++ first_ros_WS/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ali/ROS/first_ros_WS/src
+# Build directory: /home/ali/ROS/first_ros_WS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
--- first_ros_WS/devel/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig-version.cmake
+++
@@ -1,1 +0,0 @@
-/home/ali/ROS/first_ros_WS/devel/.private/catkin_tools_prebuild/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig-version.cmake
